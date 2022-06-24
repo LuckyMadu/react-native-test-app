6f076e47cbd6928cb7f5e3392bdc45365857af2c
Lahiru Amaratunge <lahiru.amaratunge@urban.com.au>
2022-06-24 16:52:03 +0530

    Added atomic components

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//components
+import {Text} from '../Text';
+//styles
+import {Container} from './Button.styles';
+
+export const Button = ({
+  disable,
+  onPress,
+  children,
+  bgColor,
+  textColor,
+  marginValue,
+  width,
+  height,
+  borderRadius,
+}) => {
+  const containerHeight = parseFloat(height);
+  return (
+    <Container
+      disabled={disable}
+      onPress={onPress}
+      bgColor={bgColor}
+      marginValue={marginValue}
+      width={width}
+      height={height}
+      borderRadius={borderRadius}>
+      <Text
+        fontSize="18px"
+        lineHeight={`${containerHeight / 2}px`}
+        fontColor={textColor}
+        fontWeight={600}>
+        {children}
+      </Text>
+    </Container>
+  );
+};
+
+Button.propTypes = {
+  disable: PropTypes.bool,
+  children: PropTypes.node,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+  marginValue: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  borderRadius: PropTypes.string,
+  onPress: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+  disable: false,
+  bgColor: '#7158e2',
+  textColor: '#ffffff',
+  marginValue: '40px 0 0 0',
+  width: '100%',
+  height: '44px',
+  borderRadius: '25px',
+};
