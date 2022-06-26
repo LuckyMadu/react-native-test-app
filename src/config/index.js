3ee95dbc010dd695ff6dd7a5ef7c3be2f69b3396
Lahiru Amaratunge <lahiru.amaratunge@urban.com.au>
2022-06-26 14:31:02 +0530

    Updated the API key

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://api.themoviedb.org/';
+const API_KEY = '32eea68e8b1885c5520a481871cbb014';
+export const API_ROOT = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
